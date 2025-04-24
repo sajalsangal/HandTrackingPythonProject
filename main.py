b9f295127afb9544fce705633c84b64c030fe09c
Sajal Sangal <sajalsangal@gmail.com>
2025-04-24 15:40:49 +0530

    Used mediapipe to draw line between hand point landmarks in real time

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ cap = cv2.VideoCapture(0) #Video Object
 
 mpHands = mp.solutions.hands
 hands = mpHands.Hands()
-
+mpDraw = mp.solutions.drawing_utils
 
 while True:
     success, img = cap.read()
@@ -18,10 +18,11 @@ while True:
 
     imgRGB = cv2.cvtColor(flipped_image, cv2.COLOR_BGR2RGB)
     results = hands.process(imgRGB)
-    print(results.multi_hand_landmarks)
+    #print(results.multi_hand_landmarks)
 
-
-
+    if results.multi_hand_landmarks:
+        for handLms in results.multi_hand_landmarks:
+            mpDraw.draw_landmarks(flipped_image , handLms, mpHands.HAND_CONNECTIONS)
 
 
     cv2.imshow("Image", flipped_image)
